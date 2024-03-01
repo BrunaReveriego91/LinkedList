76e998d8e2fefcd5d1f30a4339e8621feb338080
Bruna Reveriego <94184681+BrunaReveriego91@users.noreply.github.com>
2024-03-01 17:29:47 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace LinkedList
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<string> meses = new LinkedList<string>();
+            meses.AddLast("Março");
+            meses.AddFirst("Janeiro");
+
+            var marco = meses.Find("Março");
+
+            meses.AddBefore(marco, "Fevereiro");
+            meses.AddAfter(marco, "Abril");
+
+            var node = new LinkedListNode<string>("Maio");
+            meses.AddLast(node);
+
+            meses.Remove("Março"); //Encontra e remove o item
+            meses.Remove(node);  //Remove o node especifico
+            meses.RemoveFirst(); //Remove o primeiro item da lista
+            meses.RemoveLast(); //Remove o ultimo item da lista
+
+
+            foreach (var mes in meses)
+            {
+                Console.WriteLine(mes);
+            }
+            Console.ReadLine();
+        }
+    }
+}
